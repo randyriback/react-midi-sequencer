7b5b57119f7182402d45cd1bb1711a3ec44101bd
randy riback <randolph.riback@gmail.com>
2022-01-26 15:37:23 -0500

    fixed sort

--- src/components/Track.js
+++ src/components/Track.js
@@ -2,6 +2,7 @@ import React, { memo, useContext, useState, useEffect } from "react";
 import Note from "./Note";
 import "./components.css";
 import { Context } from "../hooks/useStore";
+import { orderBy } from "natural-orderby";
 
 const Track = ({ trackID, currentStepID, title, noteCount, onNotes }) => {
   const {
@@ -28,14 +29,15 @@ const Track = ({ trackID, currentStepID, title, noteCount, onNotes }) => {
   });
 
   const midi = useContext(Context);
-  const [disabled, setDisabled] = useState(false)
+  const [disabled, setDisabled] = useState(false);
 
   const letters = ["A", "B", "C", "D", "E", "F", "G"];
   let newArr = [];
-  let letter = letters[0];
+  let rev = [];
 
-  for (let letterIndex = 0; letterIndex < letters.length; letterIndex++) {
-    for (let octave = -1; octave < 8; octave++) {
+  for (let octave = -1; octave < 8; octave++) {
+    for (let letterIndex = 0; letterIndex < letters.length; letterIndex++) {
+      let letter = letters[letterIndex];
       if (letter === "B" || letter === "E") {
         newArr.push(letter + octave);
       } else {
@@ -45,6 +47,7 @@ const Track = ({ trackID, currentStepID, title, noteCount, onNotes }) => {
       letter = letters[letterIndex];
     }
   }
+
 
   const apiFriendly = (str) => {
     if (str.slice(2, 3) === "-" && str.slice(1, 2) === "#") {
@@ -66,15 +69,18 @@ const Track = ({ trackID, currentStepID, title, noteCount, onNotes }) => {
 
   const deviceCheck = () => {
     if (!midi.midiDevice) {
-      setDisabled(true)
-    }else{
-      setDisabled(false)
-    } 
+      setDisabled(true);
+    } else {
+      setDisabled(false);
+    }
   };
   useEffect(deviceCheck, [midi]);
 
   return (
-    <div className="track" style={disabled ? {pointerEvents: "none", opacity: "0.4"} : {}}>
+    <div
+      className="track"
+      style={disabled ? { pointerEvents: "none", opacity: "0.4" } : {}}
+    >
       <select onChange={handleSelect}>
         {newArr.map((x, index) => (
           <option value={x} key={index}>
